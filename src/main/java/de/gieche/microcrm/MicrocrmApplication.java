32184068db9f0611e0ffd0d12bb1defcf8463bac
Christian Gieche <newzealand@gieche.de>
2018-04-17 20:21:23 +0200

    Project setup

---
+++ src/main/java/de/gieche/microcrm/MicrocrmApplication.java
@@ -0,0 +1,12 @@
+package de.gieche.microcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrocrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrocrmApplication.class, args);
+    }
+}
